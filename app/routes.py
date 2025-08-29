25ac3b8ca4b7c639c9d212bc55875c340592210e
davicesarm <davicesarmorais>
2025-08-29 11:01:15 -0300

    feat: separação por campus

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 from flask import render_template
-from app.services.csv_analyze import aptos
+from app.services.csv_analyze import campi, aptos_por_campus
 
 
 def init_routes(app):
     @app.route('/')
     def home():
-        return render_template('index.html', nomes=aptos())
+        return render_template('index.html', campi=campi())
 
-    @app.route('/about')
-    def about():
-        return "About"
+    @app.route('/classificados/<campus>')
+    def about(campus):
+        return render_template('aptos_por_campus.html', campus=campus, nomes=aptos_por_campus(campus))
--- app/services/csv_analyze.py
+++ app/services/csv_analyze.py
@@ -4,7 +4,28 @@ form = pd.read_csv("form.csv")
 def nomes() -> list[str]:
     return form['Nome Completo'].to_list()
 
-def aptos():
-    return form[["Nome Completo", "Apto"]].to_records()
+def campi() -> list[str]:
+    return form["Campus (cidade) e turno:"].unique().tolist()
 
-print(aptos())
+def aptos():
+    aptos_por_campus = {}
+    
+    for campus in campi():
+        aptos_por_campus[campus] = form[(form["Campus (cidade) e turno:"] == campus)][["Carimbo de data/hora", "Nome Completo", "Apto"]].to_records().tolist()
+
+    return aptos_por_campus
+
+def aptos_por_campus(campus: str) -> list[str]:
+    form_filtrado = form[(form["Campus (cidade) e turno:"] == campus)][["Carimbo de data/hora", "Nome Completo", "Apto"]]
+
+    form_filtrado["Carimbo de data/hora"] = pd.to_datetime(
+        form_filtrado["Carimbo de data/hora"], 
+        format="%m/%d/%Y %H:%M:%S"
+    )
+
+    form_filtrado["Carimbo de data/hora"] = form_filtrado["Carimbo de data/hora"].dt.strftime("%d/%m/%Y %H:%M:%S")
+
+    return form_filtrado.to_records().tolist()
+
+
+print(aptos_por_campus("Campus Campina Grande - Turno da Tarde"))
